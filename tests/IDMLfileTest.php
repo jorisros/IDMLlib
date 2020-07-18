3a88d3c37e62cb18f5f6517fa6219bb1fc45671a
Joris Ros <joris.ros@gmail.com>
2020-07-18 17:07:25 +0200

    Add a test to composer script, and a example script to test this.

--- tests/IDMLfileTest.php
+++ tests/IDMLfileTest.php
@@ -2,8 +2,12 @@
 
 
 use JorisRos\IDMLlib\IDMLfile;
+use PHPUnit\Framework\TestCase;
 
-class IDMLfileTest extends PHPUnit_Framework_TestCase
+class IDMLfileTest extends TestCase
 {
-
+    public function testC()
+    {
+        $this->assertTrue(true);
+    }
 }
