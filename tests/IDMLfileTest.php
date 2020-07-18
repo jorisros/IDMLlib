329fc657168f277e62355065c06763371c10513e
Joris Ros <joris.ros@gmail.com>
2020-07-18 17:17:51 +0200

    Add test for reading IDML files

--- tests/IDMLfileTest.php
+++ tests/IDMLfileTest.php
@@ -3,7 +3,21 @@
 
 use JorisRos\IDMLlib\IDMLfile;
 
-class IDMLfileTest extends PHPUnit_Framework_TestCase
+class IDMLfileTest extends \PHPUnit\Framework\TestCase
 {
 
+    public function test__construct()
+    {
+        $this->expectException(\Exception::class);
+        $file = new IDMLfile('fake/path');
+        $this->assertInstanceOf(IDMLfile::class);
+    }
+
+    public function testGetContentFile()
+    {
+        $file = new IDMLfile('tests/assets/example.idml');
+        $content = $file->getContentFile('Stories/Story_udc.xml');
+
+        $this->assertNotEmpty($content);
+    }
 }
