eb7837a537e77d5c44d3e03a762f93fca1adf5d3
Joris Ros <joris.ros@gmail.com>
2020-07-19 19:32:22 +0200

    Remove not written test and update empty testwq

--- tests/JorisRos/IDMLlib/Model/MarginPreferenceTest.php
+++ tests/JorisRos/IDMLlib/Model/MarginPreferenceTest.php
@@ -16,5 +16,8 @@ class MarginPreferenceTest extends TestCase
         return new SimpleXMLElement('<MarginPreference ColumnCount="1" ColumnGutter="12" Top="36" Bottom="36" Left="36" Right="36" ColumnDirection="Horizontal" ColumnsPositions="0 211.46456692913387" />');
     }
 
-
+    public function testCompare()
+    {
+        $this->assertTrue(true);
+    }
 }
